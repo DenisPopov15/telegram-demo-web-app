bf68024d6f9184d011eb5bb425964a781f34ce37
Denis <denis@slatestudio.com>
2022-04-23 21:30:37 +0300

    Work in progress

--- src/App.js
+++ src/App.js
@@ -12,17 +12,35 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div href=''
-          // OnTouchStart={(event) => {
-          //   event.preventDefault()
-          //   const apiService = new ApiService(BOT_URL, {})
-          //   const data = 'COOLIO, ITS WORKS!!'
-          //   apiService.execute('api/demo', 'POST', { data })
-          // }}
+
+// {
+//   "button_text_color": "#ffffff",
+//   "text_color": "#ffffff",
+//   "hint_color": "#98989e",
+//   "link_color": "#007aff",
+//   "bg_color": "#1c1c1d",
+//   "button_color": "#007aff"
+// }
           onClick={(event) => {
             event.preventDefault()
-            const apiService = new ApiService(BOT_URL, {})
-            const data = 'COOLIO, ITS WORKS!!'
-            apiService.execute('api/demo', 'POST', { data })
+            const webApp = window.Telegram.WebApp
+            webApp.onEvent('themeChanged', () => {
+
+            })
+            webApp.MainButton.setText('My text')
+            webApp.MainButton.show()
+            const { initData, colorScheme, themeParams } = webApp
+            // webApp.colorScheme // light | dark
+            const orderData = { order: [{ title: 'apple', count: 2 }] }
+            const params = { initData, data: orderData }
+            webApp.MainButton.onClick(() => {
+
+              webApp.sendData(JSON.stringify(params))
+              // webApp.close()
+            })
+            // webApp.expand()
+            // const apiService = new ApiService(BOT_URL, {})
+            // apiService.execute('api/demo', 'POST', { data: params })
           }}
         >
           Click HERE!
